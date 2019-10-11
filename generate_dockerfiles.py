06229f5bc41675c9b45b94d9eb573360e7d3b7e0
Erin O'Connell <erin@occipital.com>
2019-10-11 12:23:12 -0600

    generate seperate files for python2 and python3

--- generate_dockerfiles.py
+++ generate_dockerfiles.py
@@ -1,4 +1,63 @@
-with open('templates/Dockerfile', mode='r', encoding='utf-8') as f:
-    text = f.read()
+import os
+from pathlib import Path
 
-print(text.format(image='ubuntu', tag='18.04'))
+CURRENT_DIR = Path('.')
+TEMPLATES_DIR = Path('templates/.')
+
+INSTALL_BUILD_ESSENTIALS = '''
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+        build-essentials
+    && rm -rf /var/lib/apt/lists/*
+'''
+
+PIP_VERSION = '19.2.3'
+
+images = {'ubuntu': ['18.04', '19.04']}
+
+python_versions = [
+    '2.7.16',
+    '3.5.7',
+    '3.6.9',
+    '3.7.4',
+    '3.8.0rc1',
+    '3.7.5rc1',
+    '3.5.8rc1',
+    '2.7.17rc1',
+]
+
+for image, tags in images.items():
+    for tag in tags:
+        for version in python_versions:
+            dfile = 'Dockerfile.python3' if version.startswith('3') else 'Dockerfile.python2'
+            with open(TEMPLATES_DIR / dfile, encoding='utf-8') as f:
+                dockerfile = f.read()
+
+            folder = CURRENT_DIR / version
+            os.makedirs(folder, exist_ok=True)
+
+            with open(
+                folder / f"Dockerfile.{tag.replace('.', '')}", mode='w', encoding='utf-8'
+            ) as f:
+                f.write(
+                    dockerfile.format(
+                        image=image,
+                        tag=tag,
+                        python_version=version,
+                        pip_version=PIP_VERSION,
+                        install_build_essentials='',
+                    )
+                )
+
+            if tag == '18.04' and not any(x in version for x in ('rc', 'b', 'a')):
+                with open(
+                    folder / f"Dockerfile.{tag.replace('.', '')}.dev", mode='w', encoding='utf-8'
+                ) as f:
+                    f.write(
+                        dockerfile.format(
+                            image=image,
+                            tag=tag,
+                            python_version=version,
+                            pip_version=PIP_VERSION,
+                            install_build_essentials=INSTALL_BUILD_ESSENTIALS,
+                        )
+                    )
