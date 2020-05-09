7efd713fdb1d77510109dcc6a9af9c5f0536599f
Erin O'Connell <erinocon5@gmail.com>
2020-05-09 19:53:12 +0000

    upgrade pip and new pre release versions

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ OUT = ROOT / 'out'
 
 UBUNTU_VERSIONS = {'18.04': 'bionic', '20.04': 'focal'}
 
-PYTHON_VERSIONS = {'2.7.18', '3.5.9', '3.6.10', '3.7.7', '3.8.2', '3.9.0a5'}
+PYTHON_VERSIONS = {'2.7.18', '3.5.9', '3.6.10', '3.7.7', '3.8.2', '3.8.3rc1', '3.9.0a6'}
 
 VERSIONS = ((u_ver, p_ver) for u_ver in UBUNTU_VERSIONS for p_ver in PYTHON_VERSIONS)
 
-PIP_VERSION = '20.0.2'
+PIP_VERSION = '20.1'
