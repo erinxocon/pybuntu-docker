0e3d6d4f369c5edf96f02c54210663cccd486459
Erin O'Connell <erinocon5@gmail.com>
2021-02-11 18:58:49 -0700

    update pip and build python 3.10.0a5

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ OUT = ROOT / "out"
 
 UBUNTU_VERSIONS = {"18.04": "bionic", "20.04": "focal"}
 
-PYTHON_VERSIONS = {"3.5.10", "3.6.12", "3.7.9", "3.8.7", "3.9.1", "3.10.0a4"}
+PYTHON_VERSIONS = {"3.5.10", "3.6.12", "3.7.9", "3.8.7", "3.9.1", "3.10.0a5"}
 
 VERSIONS = ((u_ver, p_ver) for u_ver in UBUNTU_VERSIONS for p_ver in PYTHON_VERSIONS)
 
-PIP_VERSION = "20.3.3"
+PIP_VERSION = "21.0.1"
