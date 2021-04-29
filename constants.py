0b2467b6a32353bdcb20fa27afd65c771bcdfd13
Erin O'Connell <erinocon5@gmail.com>
2021-04-29 01:13:17 -0600

    update pip

--- constants.py
+++ constants.py
@@ -10,4 +10,4 @@ PYTHON_VERSIONS = {"3.6.13", "3.7.10", "3.8.9", "3.9.4", "3.10.0a7"}
 
 VERSIONS = ((u_ver, p_ver) for u_ver in UBUNTU_VERSIONS for p_ver in PYTHON_VERSIONS)
 
-PIP_VERSION = "21.0.1"
+PIP_VERSION = "21.1"
